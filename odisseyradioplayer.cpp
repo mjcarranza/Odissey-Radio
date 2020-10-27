770e0cb247025dbfcc51898f3bdf68a824263537
Mario <48070830+mjcarranza@users.noreply.github.com>
2020-10-26 21:53:24 -0600

    Add files via upload
    Function to show genre`s songs.

--- odisseyradioplayer.cpp
+++ odisseyradioplayer.cpp
@@ -1,35 +1,35 @@
 #include "odisseyradioplayer.h"
 #include "ui_odisseyradioplayer.h"
 #include <QVBoxLayout>
-#include <QMediaPlayer>
-#include <QFileDialog>
 #include <fstream>
 #include <iostream>
-#include <sstream>
+#include <QMouseEvent>
+#include <QWidget>
 #include "MyLinkedList.hpp"
 
 using namespace std;
-
+auto *libs = new MyLinkedList<string>;
+QVBoxLayout *libLayout = new QVBoxLayout();
 OdisseyRadioPlayer::OdisseyRadioPlayer(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::OdisseyRadioPlayer)
 {
+    // Update user interface
     ui->setupUi(this);
+}
 
-    // assigning the pointer value to a MediaPlayer instance
-    mMediaPlayer = new QMediaPlayer(this);
+OdisseyRadioPlayer::~OdisseyRadioPlayer()
+{
+    delete ui;
+}
 
-    connect(mMediaPlayer, &QMediaPlayer::positionChanged, [&](qint64 pos) {
-      ui->SongProgress->setValue(pos);
-    });
-
-    connect(mMediaPlayer, &QMediaPlayer::durationChanged, [&] (qint64 duration){
-      ui->SongProgress->setMaximum(duration);
-    });
+// this method is exectuted when clicking a song`s genre label
+void OdisseyRadioPlayer::loadSongs(){
 
     // LinkedList instance
     auto *lines = new MyLinkedList<string>;
     string line;
+
     // Opening csv file
     ifstream myFile("/home/mario/Escritorio/fma_metadata/raw_genres.csv"); // Change to use with raw_tracks.csv
     // Reading csv file
@@ -43,7 +43,6 @@ OdisseyRadioPlayer::OdisseyRadioPlayer(QWidget *parent)
 
     // Loading song data to the UI
     int index = 1;
-    int fil = 0;
     for (int i=0; i<lines->getLen() ; i++) {
         if (4*index==i){
             // Filling out the song`s name column
@@ -64,40 +63,69 @@ OdisseyRadioPlayer::OdisseyRadioPlayer(QWidget *parent)
             ui->SongTable->setItem(ui->SongTable->rowCount()-2,3, new QTableWidgetItem(InsertingData4)); // Add data
 
             index++;
-            fil++;
         }
     }
-
 }
 
-OdisseyRadioPlayer::~OdisseyRadioPlayer()
+void OdisseyRadioPlayer::mousePressEvent(QMouseEvent *ev)
 {
-    delete ui;
-}
-void OdisseyRadioPlayer::on_PlayPauseBtn_clicked()
-{
-    if (ui->PlayPauseBtn->text() == "Play") {
-        mMediaPlayer->play();
-        ui->PlayPauseBtn->setText("Pause");
-      }
-    else {
-        mMediaPlayer->pause();
-        ui->PlayPauseBtn->setText("Play");
-      }
+    // Cursor positions
+    int X = ev->x();
+    int Y = ev->y();
+    //cout<<"esto si funciona "<<endl;
+    // Label to show each genre`s name.
+    QLabel *gLabel = new QLabel();
+    // Linked list
+
+
+    // If the Library is empty -> fill it out.
+    if(libs->getLen()==0){
+        // LinkedList instance
+        string genres;
+        // Opening csv file
+        ifstream genresFile("/home/mario/Escritorio/fma_metadata/genres.csv");
+        // Reading csv file
+        if (genresFile.is_open()) {
+            while (getline(genresFile, genres, ',')) {
+                libs->add(genres);
+            }
+            genresFile.close();
+        }
+        // If the csv File is not open.
+        else cout << "Unable to open file";
+
+
+        int index = 1;
+        // Creating a new layout for the library scroll area
+        for (int j=0; j<libs->getLen() ; j++) {
+            if (4*index==j){
+                // Filling out the song`s name column
+                gLabel = new QLabel(libs->get(j-1).data());
+                gLabel->setCursor(Qt::PointingHandCursor);
+                gLabel->setStyleSheet("background-color: white; border: 1px inset grey; min-height: 30px;");
+                libLayout->addWidget(gLabel);
+                index ++;
+            }
+        }
+        // Loading the labels to the UI
+        ui->scrollAreaContents->setLayout(libLayout);
+    }
+    else{
+        //If the library is not empty, the layout is iterated looking for the clicked widged in it
+        for (int i = 0; i < libLayout->count(); ++i){
+
+            QWidget *widget = libLayout->itemAt(i)->widget();
+
+            // If the layout structure is not empty
+            if (widget != NULL){
+
+                // Check if the mouse position is on a label
+                if (X>=widget->x()+10 && X <= widget->x()+widget->width()+10
+                        && Y>=widget->y()+30 && Y <= widget->y()+widget->height()+30 && Y<530){
+                    this->loadSongs();
+                }
+            }
+        }
+    }
 }
 
-void OdisseyRadioPlayer::on_AbrirBtn_clicked()
-{
-    QString filename = QFileDialog::getOpenFileName(this, "Abrir");
-    if (filename.isEmpty()) {
-        return;
-      }
-    mMediaPlayer->setMedia(QUrl::fromLocalFile(filename));
-    mMediaPlayer->setVolume(ui->VolumeSlider->value());
-    ui->SongNameLabel->setText(filename);
-}
-
-void OdisseyRadioPlayer::on_VolumeSlider_valueChanged(int value)
-{
-    mMediaPlayer->setVolume(value);
-}
